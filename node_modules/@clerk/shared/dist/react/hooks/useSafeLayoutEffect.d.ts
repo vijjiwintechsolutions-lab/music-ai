f60976aea4cfa6f3236a059b2e7ee982fa0a7eab
vijjiwintechsolutions-lab <vijjiwintechsolutions@gmail.com>
2026-08-07 15:38:26 +0000

    Fix dependencies and configs for build

---
+++ node_modules/@clerk/shared/dist/react/hooks/useSafeLayoutEffect.d.ts
@@ -0,0 +1,9 @@
+import React from "react";
+
+//#region src/react/hooks/useSafeLayoutEffect.d.ts
+/**
+ * @internal
+ */
+declare const useSafeLayoutEffect: typeof React.useLayoutEffect;
+//#endregion
+export { useSafeLayoutEffect };
