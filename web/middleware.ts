1eb5ebc30bd901df7b500e4d59fb279b95a15fa0
vijjiwintechsolutions-lab <vijjiwintechsolutions@gmail.com>
2026-08-08 19:10:51 +0530

    Update middleware.ts

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,22 +1,9 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/webhook(.*)",
-]);
-
-export default clerkMiddleware(
-  (auth, request) => {
-    if (!isPublicRoute(request)) {
-      auth().protect();
-    }
-  }
-);
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
