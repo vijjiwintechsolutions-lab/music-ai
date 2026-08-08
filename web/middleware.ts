1b059bc25b8ea165a2f1a6c053820c5285195bae
vijjiwintechsolutions-lab <vijjiwintechsolutions@gmail.com>
2026-08-08 19:15:20 +0530

    Update middleware.ts

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function middleware(
+  request: NextRequest
+) {
   return NextResponse.next();
 }
 
